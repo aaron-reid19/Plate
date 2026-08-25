f677ad3792cdd464dd600ff5079ac44a494d8c96
aaron <aareid1999@gmail.com>
2026-08-25 03:58:23 -0600

    scaffolded services for ci

---
+++ apps/plate-ui/app/login/loginpage.tsx
@@ -0,0 +1,7 @@
+export function LoginPage() {
+    return(
+        <main>
+            <h1>Hello</h1>
+        </main>
+    )
+}
---
+++ apps/plate-ui/components/login/loginform.tsx
@@ -0,0 +1,9 @@
+import {z} from "zod"
+import { FormHTMLAttributes } from "react"
+export function LoginForm() {
+    return(
+        <form>
+            
+        </form>
+    )
+}
